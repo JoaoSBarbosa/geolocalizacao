fa6bb60d75cbc4e68760ea7abf02ceb6f60467f6
João Barbosa <contato.jsbarbosa@gmail.com>
2023-05-02 21:58:57 -0300

    Lógica para consultar a localização

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'WolfGeo';
+
+  map_url = '';
+  latitude = 0;
+  longitude = 0;
+
+  latitude2 = 0;
+  longitude2 = 0;
+
+  ngOnInit() {
+    this.verifica_local();
+  }
+
+  verifica_local() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          this.latitude = pos.coords.latitude;
+          this.longitude = pos.coords.longitude;
+          this.map_url = `https://www.google.com/maps/search/?api=1&query=${
+            this.latitude
+          },${(this, this.longitude)}`;
+        },
+        () => {
+          alert('Sem localização!');
+        },
+        { timeout: 15000 }
+      );
+
+      const clear = document.querySelector('.limpar') as HTMLInputElement;
+      clear.addEventListener('click', () => {
+        this.latitude = 0;
+        this.longitude = 0;
+        this.map_url = '';
+      });
+    }
+  }
 }
